85ab5d7e0366c34ff07661c33f9e21ed0719fcb9
Dinesh Kumar <dinesh631991@gmail.com>
2024-10-16 02:49:03 +0530

    3

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert(succe);
+const tbody = document.querySelector('#tbody');
+
+const getdata = async () => {
+  const endpoint = "https://cem.mmtcpamp.com/pvt/getNonExecutableQuote",
+        response = await fetch(endpoint, 
+            {
+                method: 'POST',
+                body: {"currencyPair":"XAU/INR","type":"SELL"}
+            }
+        ),
+        data = await response.json(),
+        Countries = data;
+
+ Countries.forEach(countryObj => {
+    let {Country, NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered} = countryObj;
+    tbody.innerHTML += `<tr>
+        <td>${Country}</td>
+        <td>${NewConfirmed}</td>
+        <td>${TotalConfirmed}</td>
+        <td>${NewDeaths}</td>
+        <td>${TotalDeaths}</td>
+        <td>${NewRecovered}</td>
+        <td>${TotalRecovered}</td>
+    </tr>`;
+ });
+
+}
+getdata();
